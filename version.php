e81c333e7fa9548a364762516c7bb5580f971f36
Doug Cooper <douglas.cooper@qmul.ac.uk>
2014-09-08 16:40:27 +0100

    Initial creation of Blackboard Collaborate (Elluminate) block submodule

---
+++ version.php
@@ -0,0 +1,10 @@
+<?php
+$plugin->requires = 2010112400;
+$plugin->version  = 2014052101;  // The current module version (Date: YYYYMMDDxx)
+$plugin->release  = '3.1.2-0'; 	 // Human Readable version number
+$plugin->maturity = MATURITY_STABLE;
+$plugin->component = 'block_elluminate';
+
+$plugin->dependencies = array(
+   'mod_elluminate' => '2014052101'
+);
